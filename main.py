0b2ce8a6f6d1c91a348127988902427065a28dd1
rarose67 <robertrosestl67@gmail.com>
2018-03-26 19:22:09 -0500

    add template for CSS

--- main.py
+++ main.py
@@ -1,47 +1,24 @@
 from flask import Flask, request
+
 #import the alphabet_position and rotate_character functions from the helpers module 
 from helpers import alphabet_position, rotate_character
+
 #import the rotate_string function from the caesar module 
 from caesar import rotate_string
+
+import os
+import jinja2
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-form ="""
-<!DOCTYPE html>
-
-<html>
-    <head>
-        <style>
-            form {{
-                background-color: #eee;
-                padding: 20px;
-                margin: 0 auto;
-                width: 540px;
-                font: 16px sans-serif;
-                border-radius: 10px;
-            }}
-            textarea {{
-                margin: 10px 0;
-                width: 540px;
-                height: 120px;
-            }}
-        </style>
-    </head>
-    <body>
-      <form action="/encrypt" method="post">
-             <label for="rot">Rotate by:</label>
-            <input id="rot" type="text" name="rot" value=0 />
-            <textarea name="text">{0}</textarea>
-            <input type="submit" />
-        </form>
-    </body>
-</html>
-"""
-
 @app.route("/")
 def index():
-    return form.format("")
+    template = jinja_env.get_template('form.html')
+    return template.render()
 
 @app.route("/encrypt", methods=['POST'])
 def encrypt():
@@ -50,8 +27,7 @@ def encrypt():
 
     encrypted_string = rotate_string(message, rotate)
     
-    new_form = form.format(encrypted_string)
-
-    return new_form
+    template = jinja_env.get_template('form.html')
+    return template.render(message=encrypted_string)
 
 app.run()
